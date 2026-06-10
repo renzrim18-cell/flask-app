8da55fd78e3347a0fdf8af4db49a74900241bbc5
Renz Rimorin <renz.rim18@gmail.com>
2026-06-10 10:40:02 +0200

    restore

--- app.py
+++ app.py
@@ -7,9 +7,5 @@ def hello():
     name = request.args.get('name', 'World')
     return f'Hello, {name}!'
 
-@app.route('/info')
-def info():
-    return 'Flask läuft in Docker | Python Version: ' + __import__('sys').version
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
